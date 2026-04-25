af3f188290c19d2884c33640b2fb01e260c5b9f0
AkritiKushwaha12 <akritikushwaha2004@gmail.com>
2026-04-25 12:22:26 +0530

    Index of First Substring

--- indexFirstSubstring.cpp
+++ indexFirstSubstring.cpp
@@ -5,23 +5,19 @@ int strStr(string haystack, string needle) {
     int n = haystack.length();
     int m = needle.length();
 
-    // Edge case
     if (m > n) return -1;
 
     for (int i = 0; i <= n - m; i++) {
         int j = 0;
-
         // Check substring match
         while (j < m && haystack[i + j] == needle[j]) {
             j++;
         }
-
         // If full match found
         if (j == m) {
             return i;
         }
     }
-
     return -1;
 }
 
